b003f62ac4342c62335b98588e9eab0ac029750b
nordow <160596752+nordow@users.noreply.github.com>
2026-05-09 11:54:34 +0800

    perf(`weibo-bot`): set window size properly

--- src/weibo-bot/bot.py
+++ src/weibo-bot/bot.py
@@ -546,6 +546,7 @@ class Engine:
 
         options.add_argument("--no-sandbox")
         options.add_argument("--headless")
+        options.add_argument("--window-size=1920,1080")
         # options.add_argument("--incognito")
         options.add_argument("--disable-gpu")
         options.add_argument("--disable-dev-shm-usage")
@@ -554,7 +555,7 @@ class Engine:
 
         driver = webdriver.Chrome(options = options)
 
-        driver.maximize_window()
+        # driver.maximize_window()
 
         self.__driver = driver
 
